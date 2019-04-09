e8b4079b6b30d25d866ade9a654bb18cd25dfd45
dgrist <dgrist@ncsu.edu>
2019-04-09 19:29:26 -0400

    First push of the unity stuff

---
+++ Day 1/Assets/Scripts/CameraSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour
+{
+
+    public Transform player;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Day 1/Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMobility : MonoBehaviour
+{
+
+    public float speed;
+    public bool facingRight = false;  
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+      // Controls
+      float input =  Input.GetAxis("Horizontal");
+      //Animations
+      //Player Direction
+      if(input < 0.0f && facingRight){
+        FlipPlayer();
+      }
+      else if(!facingRight && input > 0.0f){
+        FlipPlayer();
+      }
+      //Physics
+      GetComponent<Rigidbody2D>().velocity = new Vector2(input * speed, GetComponent<Rigidbody2D>().velocity.y );
+    }
+
+    void FlipPlayer()
+    {
+      facingRight = !facingRight;
+      Vector2 scale = gameObject.transform.localScale;
+      scale.y *= -1;
+      transform.localScale = scale;
+    }
+}
